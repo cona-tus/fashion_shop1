330ea0514329312305df0575f529d413daedab7b
cona-tus <cam_panella@naver.com>
2022-02-09 14:22:33 +0900

    slideTitle fade-in

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const slides = document.querySelectorAll(".slide");
 const totalSlides = slides.length;
+const slideText = document.querySelectorAll(".slide__text");
 let slideIndex = 0;
+const SHOWING_CLASS = "showing";
 
 window.addEventListener("load", () => {
+  init();
   setInterval(function () {
     plusSlides(1);
   }, 5000);
 });
+
+function init() {
+  const firstSlide = document.querySelector(".slide");
+  const firstTitle = document.querySelector(".slide__title");
+  firstSlide.classList.add(SHOWING_CLASS);
+  firstTitle.style.animation = "fade-in 0.5s ease-in";
+}
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
@@ -20,10 +30,19 @@ function showSlides(n) {
     slideIndex = 0;
   }
   slides.forEach(function (element) {
-    element.classList.remove("showing");
+    element.classList.remove(SHOWING_CLASS);
   });
-  slides[slideIndex].classList.add("showing");
-  console.log(slideIndex);
+  slides[slideIndex].classList.add(SHOWING_CLASS);
+  slideAnimation();
+}
+
+function slideAnimation() {
+  const slideTitle = document.querySelectorAll(".slide__title");
+
+  slideTitle.forEach(function (element) {
+    element.style.animation = "";
+  });
+  slideTitle[slideIndex].style.animation = "fade-in 0.5s ease-in";
 }
 
 const nextBtn = document.getElementById("next");
