f21bc6a640f920d2013b6dd407c6530cd9437213
cona-tus <cam_panella@naver.com>
2022-03-02 08:00:40 +0900

    CSS : product info

--- main.js
+++ main.js
@@ -1,3 +1,82 @@
 "use strict";
 
-const lookbookImgs = document.querySelectorAll(".lookbook__img li");
+// const trendingBtnPrev = document.querySelector("#trending__prev");
+// const trendingBtnNext = document.querySelector("#trending__next");
+// const trendingItems = document.querySelector(".trending__items");
+// let initial = 0;
+// let amount = 50;
+
+// trendingBtnNext.addEventListener("click", () => {
+//   initial -= amount;
+//   trendingItems.style.transform = "translateX(" + initial + "vw)";
+//   console.log(initial);
+
+//   if (initial !== -100) {
+//     trendingBtnNext.classList.remove("hidden");
+//     trendingBtnPrev.classList.remove("hidden");
+//   } else {
+//     trendingBtnNext.classList.add("hidden");
+//   }
+// });
+
+// trendingBtnPrev.addEventListener("click", () => {
+//   trendingBtnPrev.classList.add("hidden");
+//   initial += amount;
+//   trendingItems.style.transform = "translateX(" + initial + "vw)";
+//   console.log(initial);
+
+//   if (initial !== 0) {
+//     trendingBtnPrev.classList.remove("hidden");
+//     trendingBtnNext.classList.remove("hidden");
+//   } else {
+//     trendingBtnNext.classList.remove("hidden");
+//   }
+// });
+
+// // navbar
+// const shopNavbar = document.querySelector(".shop__navbar");
+// const shopButton = document.querySelector(".shop__button");
+// const shopMenu = document.querySelector(".shop__menu");
+
+// shopButton.addEventListener("mouseover", () => {
+//   console.log("hi");
+//   shopMenu.classList.add("active");
+// });
+
+// shopButton.addEventListener("mouseleave", () => {
+//   console.log("bye");
+//   shopMenu.classList.remove("active");
+// });
+
+// filter
+
+const categoryBtnContainer = document.querySelector(".category__filter");
+const productContainer = document.querySelector(".product");
+const products = document.querySelectorAll(".product__img");
+const shopInfo = document.querySelectorAll(".shop__info");
+
+categoryBtnContainer.addEventListener("click", (e) => {
+  console.log("click");
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  const active = document.querySelector(".category__button.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  e.target.classList.add("selected");
+
+  productContainer.classList.add("anim-out");
+  setTimeout(() => {
+    products.forEach((product) => {
+      if (filter === "*" || filter === product.dataset.type) {
+        product.classList.remove("invisible");
+      } else {
+        product.classList.add("invisible");
+      }
+    });
+    productContainer.classList.remove("anim-out");
+  }, 300);
+});
