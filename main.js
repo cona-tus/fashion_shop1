7eb36ccf6b46c2e5a9917f57dfc0826cb231cf9f
cona-tus <cam_panella@naver.com>
2022-03-03 13:06:38 +0900

    FAQ page create

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
 "use strict";
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.56353484183652, lng: 126.9783009492541 },
-    zoom: 20,
-  });
-}
-
-initMap();
