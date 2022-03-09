72eeefae834316ec5a7b7bd04fcafd50ce03d38a
cona-tus <cam_panella@naver.com>
2022-03-09 20:56:41 +0900

    a link

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const searchButton = document.querySelector(".search__icon");
+searchButton.addEventListener("click", () => {
+  const searchScreen = document.querySelector(".search__screen");
+  searchScreen.classList.add("active");
+});
+
+const closeButton = document.querySelector(".close__icon");
+closeButton.addEventListener("click", () => {
+  const searchScreen = document.querySelector(".search__screen");
+  searchScreen.classList.remove("active");
+});
