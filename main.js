c804c5eb7a558306fe0bee596caacac57ee99fbf
cona-tus <cam_panella@naver.com>
2022-03-10 16:24:26 +0900

    index page code organize

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// navbar search button
 const searchButton = document.querySelector(".search__icon");
 searchButton.addEventListener("click", () => {
   const searchScreen = document.querySelector(".search__screen");
@@ -10,4 +11,21 @@ const closeButton = document.querySelector(".close__icon");
 closeButton.addEventListener("click", () => {
   const searchScreen = document.querySelector(".search__screen");
   searchScreen.classList.remove("active");
+});
+
+// main scroll slide-in animation
+const lookFirstItem = document.querySelector(".lookbook__item:nth-child(1)");
+const lookLastItem = document.querySelector(".lookbook__item:nth-child(4)");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  console.log(value);
+
+  if (value > 1800) {
+    lookFirstItem.style.animation = "slide-in 0.5s ease-in forwards";
+  }
+
+  if (value > 2500) {
+    lookLastItem.style.animation = "slide-in 0.5s ease-in forwards";
+  }
 });
